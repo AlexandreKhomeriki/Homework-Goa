bae99cb22e209d2ba8dcbc7990a947b24dab5df3
AleksandreKhomeriki <khomera23@gmail.com>
2024-08-17 14:36:23 +0400

    Commit

---
+++ Level 23/Homework.py
@@ -0,0 +1,8 @@
+data_list = [42, "Hello", 3.14, True, None, [1, 2, 3], {"key": "value"}, (7, 8), {9, 10}, b'byte_data']
+
+# 5 მონაცემის მნიშვნელობის შეცვლა
+data_list[0] = 99        
+data_list[1] = "World"    
+data_list[2] = 2.718      
+data_list[3] = False      
+data_list[4] = "Not None" 
