878f720073bb6b1e05458a44a80636f3813c4f79
AleksandreKhomeriki <khomera23@gmail.com>
2024-10-12 11:12:55 +0400

    Commit

---
+++ Level 43/Homework/Problem2.py
@@ -0,0 +1,7 @@
+#Task 3: Joining Strings
+#Create a list named words that contains the following strings: "apple", "banana", "cherry".
+#Use the join() function to combine these words into a single string, separated by a comma and a space ", ". Store the result in a variable named fruit_string and print it.
+
+words = ["apple", "banana", "cherry"]
+fruit_string = ", ".join(words)
+print(fruit_string)
---
+++ Level 43/Homework/Problem5.py
@@ -0,0 +1,7 @@
+#Task 6: Converting to Lowercase
+#Create a string named mixed_case with the following content: "PyThOn Is AwEsOmE!"
+# the lower() function to convert the entire string to lowercase. Store the result in a variable named lowercase_string and print it.
+
+mixed_case = "PyThOn Is AwEsOmE!"
+lowercase_string = mixed_case.lower()
+print(lowercase_string)
